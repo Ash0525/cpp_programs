4258fd1678bfd0d18be7c9cadea30697eed88448
Alexander Tiu <alextiu0525@gmail.com>
2026-07-10 22:53:39 -0500

    Codex made it cleaner but now we have to run with ./run.sh

--- chargeLab/run.bat
+++ chargeLab/run.bat
@@ -1,12 +1,3 @@
 @echo off
 setlocal
-set PATH=%CD%\lib;%PATH%
-echo Starting ChargeLab...
-run.exe
-if errorlevel 1 (
-    echo.
-    echo Error occurred! Press any key to close...
-    pause
-) else (
-    echo Program closed successfully
-)
+call "%~dp0run.cmd"
--- chargeLab/run.sh
+++ chargeLab/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH="$PWD/lib:$LD_LIBRARY_PATH"
 export PATH="$PWD/lib:$PATH"
-./run.exe
+./app.exe
